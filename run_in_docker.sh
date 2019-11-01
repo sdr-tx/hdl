5156b139cb4bc4ad674923d397351b754b89588f
Agustin Omar Diaz Antuña <diazantun@est.frba.utn.edu.ar>
2019-11-01 00:11:16 -0300

    Docker with GNURadio
    There are some problems with compilation workflow

--- run_in_docker.sh
+++ run_in_docker.sh
@@ -1,13 +1,29 @@
 #!/usr/bin/env bash
 GIT_DIR=$(git rev-parse --show-toplevel)
 docker build -f $GIT_DIR/docker/Dockerfile -t icestorm .
+
 docker run -it --rm \
-           -v$PWD:$PWD \
-           -v$GIT_DIR:$GIT_DIR \
-           -v/dev/:/dev/ \
+           -v $PWD:$PWD \
+           -v $GIT_DIR:$GIT_DIR \
+           -v ${HOME}:${HOME} \
+           -v /dev/:/dev/ \
+           -v /etc/passwd:/etc/passwd:ro \
+           -v /etc/shadow:/etc/shadow:ro \
+           -v /etc/group:/etc/group:ro \
+           -v /tmp:/tmp \
+           -v /tmp/.X11-unix:/tmp/.X11-unix \
+           -v /var/run/dbus:/var/run/dbus \
+           -v /usr/share/git/completion:/usr/share/git/completion \
+           -h $(hostname) \
            -u$(id -u):$(id -g) \
+           -e DISPLAY=$DISPLAY \
            --privileged \
-           -w$PWD \
+           -i -w $PWD -t -u $(id -u):$(id -g) --rm \
+           --group-add=plugdev \
+           --group-add=sudo \
            icestorm \
-           ${@:-/bin/bash}
+           /bin/bash
+
+
+
 
